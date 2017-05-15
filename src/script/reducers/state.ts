74d2a5580644b91e6c4fcb5265bcd76468f97027
hejny <me@pavolhejny.com>
2017-05-15 02:16:32 +0200

    data model - redux action types

--- src/script/reducers/state.ts
+++ src/script/reducers/state.ts
@@ -10,9 +10,19 @@ export function stateReducer(state,action){
 
 
 
-        case 'CHANGE_VALUE':
+        case 'CHANGE_NAME':
 
             return state.set('value',action.value);
+
+        case 'CHANGE_DESCRIPTION':
+        case 'CHANGE_STARTING_DATE':
+
+
+        case 'OPTION_ADD':
+        case 'OPTION_CHANGE_KEY':
+        case 'OPTION_DELETE':
+
+
 
 
         default:
