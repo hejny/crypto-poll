00eac214db73889fb5b93ae0304b92369ecdd91d
hejny <me@pavolhejny.com>
2017-05-15 02:27:01 +0200

    data model - redux action types

--- src/script/reducers/state.ts
+++ src/script/reducers/state.ts
@@ -12,17 +12,29 @@ export function stateReducer(state,action){
 
         case 'CHANGE_NAME':
 
-            return state.set('value',action.value);
+            return state.set('name',action.name);
 
         case 'CHANGE_DESCRIPTION':
+
+            return state.set('description',action.name);
+
+
         case 'CHANGE_STARTING_DATE':
+
+            return state.set('starting_date',action.starting_date.getTime());
 
 
         case 'OPTION_ADD':
+
+            return state.update('options',(options)=>options.push(Immutable.fromJS(action.option)));
+
         case 'OPTION_CHANGE_KEY':
+
+            return state.setIn(['options',action.option_index,action.key],action.value);
+
         case 'OPTION_DELETE':
 
-
+            return state.deleteIn(['options',action.option_index]);
 
 
         default:
