69ab751e903326f86c87e94b2f2fb51e52a444b7
hejny <me@pavolhejny.com>
2017-05-16 02:26:49 +0200

    view - root component

--- src/script/components/root.tsx
+++ src/script/components/root.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as FontAwesome from 'react-fontawesome';
 
+import {EMPTY_OPTION} from '../config.ts';
 
 
 export function RootComponent(props) {
@@ -19,10 +20,23 @@ export function RootComponent(props) {
 
 
             <ul>
+                {stateJS.options.map((option)=>(
+                    <li>
+                        {option.name} -
+                        {option.address}
 
+                    </li>
+                ))}
+            </ul>
+
+
+            <button onClick={()=>store.dispatch({type:'OPTION_ADD',option:EMPTY_OPTION})}>
+                <FontAwesome name="plus"/>
+                Add
+            </button>
 
 
         </div>
-    );
+);
 
 }
--- src/script/config.ts
+++ src/script/config.ts
@@ -1,2 +1,9 @@
 export const WEB_NAME = 'Crypto poll';
 export const TITLE_SEPARATOR = ' | ';
+
+
+export const EMPTY_OPTION = {
+    name: '',
+    address: '',
+    address_type: 'BTC',
+};
